e3b4fbfc412e7524aff9acf9437e723e9e5f20b7
Simon9124 <435776313@qq.com>
2019-07-23 16:51:17 +0800

    main.js解锁实际打包时也使用mock

--- src/main.js
+++ src/main.js
@@ -8,7 +8,9 @@ import iView from 'iview'
 import i18n from '@/locale'
 import config from '@/config'
 import importDirective from '@/directive'
-import { directive as clickOutside } from 'v-click-outside-x'
+import {
+  directive as clickOutside
+} from 'v-click-outside-x'
 import installPlugin from '@/plugin'
 import './index.less'
 import '@/assets/icons/iconfont.css'
@@ -17,7 +19,7 @@ import VOrgTree from 'v-org-tree'
 import 'v-org-tree/dist/v-org-tree.css'
 // 实际打包时应该不引入mock
 /* eslint-disable */
-if (process.env.NODE_ENV !== 'production') require('@/mock')
+// if (process.env.NODE_ENV !== 'production') require('@/mock')
 
 Vue.use(iView, {
   i18n: (key, value) => i18n.t(key, value)

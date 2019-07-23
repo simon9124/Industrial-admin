aa71bf8d1c0121590b38207af9c5f3aa12e91ffc
Simon9124 <435776313@qq.com>
2019-07-23 16:16:16 +0800

    取消router的history模式

--- src/router/index.js
+++ src/router/index.js
@@ -3,20 +3,33 @@ import Router from 'vue-router'
 import routes from './routers'
 import store from '@/store'
 import iView from 'iview'
-import { setToken, getToken, canTurnTo, setTitle } from '@/libs/util'
+import {
+  setToken,
+  getToken,
+  canTurnTo,
+  setTitle
+} from '@/libs/util'
 import config from '@/config'
-const { homeName } = config
+const {
+  homeName
+} = config
 
 Vue.use(Router)
 const router = new Router({
-  routes,
-  mode: 'history'
+  routes
+  // 部署到github gh-pages -> 取消history
+  // mode: 'history'
 })
 const LOGIN_PAGE_NAME = 'login'
 
 const turnTo = (to, access, next) => {
   if (canTurnTo(to.name, access, routes)) next() // 有权限，可访问
-  else next({ replace: true, name: 'error_401' }) // 无权限，重定向到401页面
+  else {
+    next({
+      replace: true,
+      name: 'error_401'
+    })
+  } // 无权限，重定向到401页面
 }
 
 router.beforeEach((to, from, next) => {

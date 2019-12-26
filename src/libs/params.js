c6e5800e84319bc9e69890b1b28c4e63e4f2cbeb
simon9124 <435776313@qq.com>
2019-12-26 15:54:49 +0800

    封装方法添加注释

--- src/libs/params.js
+++ src/libs/params.js
@@ -1,3 +1,7 @@
+/**
+ * 路由跳转时，将router的params存储到localStorage，再从localStorage中获取数据
+ * @param {String} key 要存储在localStorage的字段
+ */
 export function params(_this, key) {
   if (_this.$route.params[key]) {
     localStorage.setItem(key, _this.$route.params[key]);
@@ -9,6 +13,10 @@ export function params(_this, key) {
   }
 }
 
+/**
+ * 删除localStorage里的数据
+ * @param {String} key 要删除的字段
+ */
 export function clearParam(key) {
   localStorage.removeItem(key);
 }

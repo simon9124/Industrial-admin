16cd0eade068cdb29b9b15fe0198f737235bd55a
simon9124 <435776313@qq.com>
2020-04-09 11:42:24 +0800

    账号管理：模拟事件 & 模拟数据

--- src/view/3manage/account.vue
+++ src/view/3manage/account.vue
@@ -44,50 +44,30 @@
             :label-width="100"
             @submit.native.prevent>
         <FormItem label="账号："
-                  prop="user_name">
+                  prop="userName">
           <Input type="text"
-                 v-model.trim="modalData.user_name"
-                 :disabled="userAccess[0]==='workshop_manager' && modalData.userAccess==='workshop_manager'"></Input>
+                 v-model.trim="modalData.userName"></Input>
+          <!-- :disabled="roles[0]==='workshop_manager' && modalData.roles==='workshop_manager'" -->
         </FormItem>
         <FormItem label="姓名："
-                  prop="display_name">
+                  prop="displayName">
           <Input type="text"
-                 v-model.trim="modalData.display_name"></Input>
+                 v-model.trim="modalData.displayName"></Input>
         </FormItem>
         <FormItem label="手机号："
-                  prop="user_phone"
+                  prop="phone"
                   class="phone">
           <Input type="text"
-                 v-model.trim="modalData.user_phone"></Input>
-        </FormItem>
-        <FormItem label="用户组："
-                  prop="group_id">
-          <Select v-model="modalData.group_id">
-            <Option v-for="group in userGroup"
-                    :value="group.group_id"
-                    :key="group.group_id">{{ group.group_name }}</Option>
-          </Select>
+                 v-model.trim="modalData.phone"></Input>
         </FormItem>
         <FormItem label="角色："
-                  prop="userAccess">
-          <!-- <Select v-model="modalData.user_access"
-                  @on-change="accessOnChange">
-            multiple
-            :max-tag-count="3"
-            <Option value="admin"
-                    :disabled="userAccess[0]!=='admin'">管理员</Option>
-            <Option value="cestc"
-                    :disabled="userAccess[0]!=='admin'&&userAccess[0]!=='cestc'">工程师</Option>
-            <Option value="workshop_manager"
-                    :disabled="userAccess[0]==='proline_leader'||userAccess[0]==='examine'">车间主管</Option>
-            <Option value="proline_leader"
-                    :disabled="userAccess[0]==='examine'">产线线长</Option>
-            <Option value="examine">检测员</Option>
-          </Select> -->
-          <Select v-model="modalData.user_access"
-                  @on-change="accessOnChange">
+                  prop="roles">
+          <Select v-model="modalData.roles"
+                  @on-change="accessOnChange"
+                  multiple
+                  :max-tag-count="3">
             <Option v-for="(role,i) in roleList"
-                    :value="role.name"
+                    :value="role.id"
                     :key="i">
               {{ role.title }}
             </Option>
@@ -112,15 +92,6 @@ import {
   userList, // 用户列表
   roleList // 角色列表
 } from "./mockData/role";
-// api
-import {
-  getUserList,
-  insertUser,
-  updateUser,
-  deleteUser
-} from "@/api/user/index";
-import { getUseGroupList } from "@/api/userGroup/index"; // 获取用户组列表
-import { getRolelist } from "@/api/role/index"; // 获取角色列表
 // function
 import { validateTel } from "@/libs/validate"; // 手机号验证
 import {
@@ -128,49 +99,63 @@ import {
   getValueByKey, // 根据对象数组某个key的value，查询另一个key的value
   resultCallback // 根据请求的status执行回调函数
 } from "@/libs/dataHanding";
+// api
+import {
+  getUserList,
+  insertUser,
+  updateUser,
+  deleteUser,
+  lockUser
+} from "@/api/user/index"; // 用户增删改查
+import { getRolelist } from "@/api/role/index"; // 获取角色列表
 
 export default {
   data() {
     return {
       /* 全局 */
       roleList: [], // 全部角色列表 - select用
-      userGroup: [], // 用户组列表 - select用
       /* table */
       tableDataOrg: [], // 原始数据
       tableData: [], // 处理后的当页数据
       tableColumns: [
         {
           title: "账号",
-          key: "user_name",
+          key: "userName",
           align: "center",
           minWidth: 120
         },
         {
           title: "姓名",
-          key: "display_name",
+          key: "displayName",
           align: "center",
           minWidth: 120
         },
         {
           title: "电话",
-          key: "user_phone",
+          key: "phone",
           align: "center",
           minWidth: 120
         },
         {
           title: "角色",
-          key: "userAccess",
+          key: "roles",
           align: "center",
           render: (h, params) => {
-            return h(
-              "div",
-              getValueByKey(
-                this.roleList,
-                "name",
-                params.row.userAccess,
-                "title"
-              )
-            );
+            return h("div", [
+              params.row.roles.map(item => {
+                return h(
+                  "Tag",
+                  {
+                    props: {
+                      color: "blue"
+                    }
+                  },
+                  !this.isMock
+                    ? item.title
+                    : getValueByKey(this.roleList, "id", item.id, "title")
+                );
+              })
+            ]);
           },
           minWidth: 120
         },
@@ -215,7 +200,7 @@ export default {
                 {
                   props: {
                     trigger: "hover",
-                    content: params.row.lock_flag === "0" ? "锁定" : "解锁",
+                    content: params.row.lockFlag === 0 ? "锁定" : "解锁",
                     placement: "top",
                     transfer: true
                   }
@@ -226,7 +211,7 @@ export default {
                       type: "warning",
                       size: "small",
                       icon:
-                        params.row.lock_flag === "0"
+                        params.row.lockFlag === 0
                           ? "ios-key-outline"
                           : "ios-key"
                     },
@@ -280,17 +265,15 @@ export default {
       modalShow: false, // 是否显示
       modalDataType: "", // 类型 - insert or edit
       modalData: {
-        user_name: "",
-        display_name: "",
-        user_phone: "",
-        group_id: "",
-        user_access: "",
-        lock_flag: "0",
-        user_avator: ""
+        userName: "",
+        displayName: "",
+        phone: "",
+        roles: [],
+        lockFlag: 0
       }, // 数据
       modalDataOrg: {}, // 数据 - 行内原始
       formModalRule: {
-        user_name: [
+        userName: [
           {
             required: true,
             message: "请输入账户名",
@@ -298,7 +281,7 @@ export default {
           },
           { type: "string", max: 20, message: "账户名过长", trigger: "change" }
         ],
-        display_name: [
+        displayName: [
           {
             required: true,
             message: "请输入姓名",
@@ -306,7 +289,7 @@ export default {
           },
           { type: "string", max: 10, message: "姓名过长", trigger: "change" }
         ],
-        user_phone: [
+        phone: [
           {
             required: true,
             trigger: "change",
@@ -319,14 +302,11 @@ export default {
             }
           }
         ],
-        // group_id: [
-        //   { required: true, message: "请选择用户组", trigger: "change" }
-        // ],
-        userAccess: [
+        roles: [
           {
             required: true,
             validator: function(rule, value, callback) {
-              if (value === "") {
+              if (value.length === 0) {
                 callback(new Error("请选择用户角色"));
               } else {
                 callback();
@@ -344,9 +324,6 @@ export default {
   },
   async created() {
     this.getData();
-    this.userGroup = !this.isMock
-      ? (await getUseGroupList()).data.data || []
-      : [];
     this.roleList = !this.isMock
       ? (await getRolelist()).data.data || []
       : roleList;
@@ -367,25 +344,25 @@ export default {
         this.refreshData();
         this.buttonLoading = false;
       }
-      // 车间主管只能看检测员、产线线长和自己信息
-      if (this.userAccess[0] === "workshop_manager") {
-        for (let i = this.tableDataOrg.length - 1; i >= 0; i--) {
-          if (
-            this.tableDataOrg[i].userAccess === "admin" ||
-            this.tableDataOrg[i].userAccess === "cestc" ||
-            (this.tableDataOrg[i].userAccess === "workshop_manager" &&
-              this.tableDataOrg[i].user_name !== this.userName)
-          ) {
-            this.tableDataOrg.splice(i, 1);
-          }
-        }
-      }
     },
     // 根据条件刷新数据
     refreshData() {
       if (this.isMock) {
-        // 按"user_name"升序
-        this.tableDataOrg.sort(arraySort("user_name", "asc"));
+        // 按"userName"升序
+        this.tableDataOrg.sort(arraySort("userName", "asc"));
+        // 车间主管只能看检测员、产线线长和自己信息
+        if (this.userAccess[0] === "workshop_manager") {
+          for (let i = this.tableDataOrg.length - 1; i >= 0; i--) {
+            if (
+              this.tableDataOrg[i].userAccess[0].name === "admin" ||
+              this.tableDataOrg[i].userAccess[0].name === "cestc" ||
+              (this.tableDataOrg[i].userAccess[0].name === "workshop_manager" &&
+                this.tableDataOrg[i].userName !== this.userName)
+            ) {
+              this.tableDataOrg.splice(i, 1);
+            }
+          }
+        }
       }
       // 分页 & 每页条数
       this.tableData = this.tableDataOrg.slice(
@@ -413,7 +390,6 @@ export default {
     insert() {
       this.modalDataType = "insert";
       this.$refs.formModalData.resetFields();
-      this.modalData.user_access = ["examine"];
       this.modalShow = true;
     },
     // 点击按钮 - 编辑
@@ -421,16 +397,22 @@ export default {
       this.modalDataType = "edit";
       this.modalDataOrg = row;
       this.modalData = JSON.parse(JSON.stringify(row));
-      // this.modalData.group_id =
-      //   this.modalData.group_name === "开发组" ? "001" : "002";
+      const roles = [];
+      this.modalDataOrg.roles.forEach(role => {
+        roles.push(role.id);
+      });
+      this.modalData.roles = roles;
       this.modalShow = true;
     },
     // 用户角色被选择
     accessOnChange(value) {
-      // console.log(value);
-      // this.modalData.userAccess = value.join(",");
-      this.modalData.userAccess = value;
-      this.modalData.user_access = [this.modalData.user_access];
+      // this.modalData.roles = value.join(",");
+      // const roles = [];
+      // value.forEach(role => {
+      //   roles.push({ id: role });
+      // });
+      // this.modalData.roles = roles;
+      // this.modalData.roles = [this.modalData.roles];
     },
     // 点击表单按钮 - 确定
     handleSubmit() {
@@ -438,16 +420,22 @@ export default {
       this.$refs.formModalData.validate(async valid => {
         if (valid) {
           this.buttonLoading = true;
-          this.modalData.user_avator = "";
+          // 处理roles的格式，赋给新对象
+          const modalData = JSON.parse(JSON.stringify(this.modalData));
+          const roles = [];
+          this.modalData.roles.forEach(role => {
+            roles.push({ id: role });
+          });
+          modalData.roles = roles;
+          // console.log(modalData);
           switch (this.modalDataType) {
             case "insert":
-              this.modalData.userAccess = this.modalData.user_access.join(",");
               if (!this.isMock) {
                 // 非mock时
-                if (this.modalData.user_phone === undefined) {
-                  this.modalData.user_phone = "";
+                if (this.modalData.phone === undefined) {
+                  this.modalData.phone = "";
                 }
-                const result = (await insertUser(this.modalData)).data.status;
+                const result = (await insertUser(modalData)).data.status;
                 resultCallback(
                   result,
                   "添加成功！",
@@ -463,7 +451,7 @@ export default {
                 // mock时
                 if (
                   this.tableDataOrg.some(
-                    item => item.user_name === this.modalData.user_name
+                    item => item.userName === this.modalData.userName
                   )
                 ) {
                   // 判断重复
@@ -471,12 +459,10 @@ export default {
                   this.buttonLoading = false;
                 } else {
                   // 随机生成sop的id
-                  this.modalData.user_id = Math.random()
+                  modalData.id = Math.random()
                     .toString(36)
                     .substr(-10);
-                  this.tableDataOrg.push(
-                    JSON.parse(JSON.stringify(this.modalData))
-                  );
+                  this.tableDataOrg.push(JSON.parse(JSON.stringify(modalData)));
                   resultCallback(200, "添加成功！", () => {
                     this.refreshData();
                     this.buttonLoading = false;
@@ -487,8 +473,8 @@ export default {
               break;
             case "edit":
               if (!this.isMock) {
-                // 非mock时
-                const result = (await updateUser(this.modalData)).data.status;
+                /* 非mock时 */
+                const result = (await updateUser(modalData)).data.status;
                 resultCallback(
                   result,
                   "修改成功！",
@@ -504,9 +490,9 @@ export default {
                 // mock时
                 if (
                   this.tableDataOrg.some(
-                    item => item.user_name === this.modalData.user_name
+                    item => item.userName === this.modalData.userName
                   ) &&
-                  this.modalData.user_name !== this.modalDataOrg.user_name
+                  this.modalData.userName !== this.modalDataOrg.userName
                 ) {
                   // 判断重复
                   this.$Message.error("该账号已存在！");
@@ -515,7 +501,7 @@ export default {
                   this.$set(
                     this.tableDataOrg,
                     (this.pageNum - 1) * this.pageSize + this.modalData._index,
-                    JSON.parse(JSON.stringify(this.modalData))
+                    JSON.parse(JSON.stringify(modalData))
                   );
                   resultCallback(200, "修改成功！", () => {
                     this.refreshData();
@@ -531,13 +517,13 @@ export default {
     },
     // 点击按钮 - 锁定/解锁
     async lock(row) {
-      row.lock_flag = row.lock_flag === "0" ? "1" : "0";
+      row.lockFlag = row.lockFlag === 0 ? 1 : 0;
       if (!this.isMock) {
         // 非mock时
-        const result = (await updateUser(row)).data.status;
+        const result = (await lockUser(row)).data.status;
         resultCallback(
           result,
-          row.lock_flag === "1" ? "锁定成功！" : "解锁成功！",
+          row.lockFlag === 1 ? "锁定成功！" : "解锁成功！",
           () => {
             this.getData();
           }
@@ -549,9 +535,7 @@ export default {
           (this.pageNum - 1) * this.pageSize + row._index,
           JSON.parse(JSON.stringify(row))
         );
-        this.$Message.success(
-          row.lock_flag === "1" ? "锁定成功！" : "解锁成功！"
-        );
+        this.$Message.success(row.lockFlag === 1 ? "锁定成功！" : "解锁成功！");
       }
     },
     // 点击按钮 - 删除
@@ -561,7 +545,7 @@ export default {
         onOk: async () => {
           if (!this.isMock) {
             // 非mock时
-            const result = (await deleteUser(row.user_id)).data.status;
+            const result = (await deleteUser(row.id)).data.status;
             resultCallback(result, "删除成功！", () => {
               this.getData();
             });
@@ -573,7 +557,7 @@ export default {
                 this.pageNum * this.pageSize
               )
               .forEach((item, i) => {
-                if (row.user_id === item.user_id) {
+                if (row.id === item.id) {
                   this.tableDataOrg.splice(
                     (this.pageNum - 1) * this.pageSize + i,
                     1
--- src/view/3manage/mockData/role.js
+++ src/view/3manage/mockData/role.js
@@ -1,158 +1,148 @@
 // 用户列表
 const userList = [
   {
-    id: null,
-    user_id: "dea72145-1ff2-42de-ae94-6d936a74ba5c",
-    user_name: "admin",
-    display_name: "管理员",
-    user_phone: "13888888888",
-    user_avator: "",
-    user_pwd: "2019@cestc",
-    user_access: [1],
-    userAccess: "admin",
-    group_id: "7",
-    group_name: "产品组",
-    lock_flag: "0"
+    id: "dea72145-1ff2-42de-ae94-6d936a74ba5c",
+    userName: "admin",
+    displayName: "管理员",
+    phone: "13888888888",
+    roles: [
+      {
+        id: "1"
+      }
+    ],
+    lockFlag: 0
   },
   {
-    id: null,
-    user_id: "eec11929-3c60-497b-942c-92a3dd3eeeb5",
-    user_name: "cestc",
-    display_name: "工程师",
-    user_phone: "13801380138",
-    user_avator: "",
-    user_pwd: "2019@cestc",
-    user_access: ["cestc"],
-    userAccess: "cestc",
-    group_id: "4",
-    group_name: "静音检测站",
-    lock_flag: "0"
+    id: "eec11929-3c60-497b-942c-92a3dd3eeeb5",
+    userName: "cestc",
+    displayName: "工程师",
+    phone: "13801380138",
+    roles: [
+      {
+        id: "2"
+      }
+    ],
+    lockFlag: 0
   },
   {
-    id: null,
-    user_id: "ada9bf57-99ad-4ca6-8000-5b245c59af3d",
-    user_name: "cgh",
-    display_name: "陈国华",
-    user_phone: "15910512300",
-    user_avator: "",
-    user_pwd: "2019@cestc",
-    user_access: ["cestc"],
-    userAccess: "cestc",
-    group_id: "1",
-    group_name: "",
-    lock_flag: "0"
+    id: "ada9bf57-99ad-4ca6-8000-5b245c59af3d",
+    userName: "gy",
+    displayName: "gy",
+    phone: "15210533478",
+    roles: [
+      {
+        id: "1"
+      }
+    ],
+    lockFlag: 0
   },
   {
-    id: null,
-    user_id: "f706830e-9b9f-498e-b1d9-536d7c366313",
-    user_name: "chenguohua1",
-    display_name: "陈国华1",
-    user_phone: "15910512300",
-    user_avator: "",
-    user_pwd: "123456",
-    user_access: ["examine"],
-    userAccess: "examine",
-    group_id: "d2e5ce63-0e12-403a-b144-cfc85782709f",
-    group_name: "",
-    lock_flag: "0"
+    id: "f706830e-9b9f-498e-b1d9-536d7c366313",
+    userName: "dj",
+    displayName: "dj",
+    phone: "15210533478",
+    roles: [
+      {
+        id: "1"
+      }
+    ],
+    lockFlag: 0
   },
   {
-    id: null,
-    user_id: "9d974bb4-2386-4bb8-98c5-499b95ad025c",
-    user_name: "examine",
-    display_name: "检测员",
-    user_phone: "",
-    user_avator: "",
-    user_pwd: "2019@cestc",
-    user_access: ["examine"],
-    userAccess: "examine",
-    group_id: "3",
-    group_name: "综合检测站",
-    lock_flag: "0"
+    id: "ddccd51f-6054-470d-91b1-b63785ccb528",
+    userName: "dn",
+    displayName: "dn",
+    phone: "15210533478",
+    roles: [
+      {
+        id: "1"
+      }
+    ],
+    lockFlag: 0
   },
   {
-    id: null,
-    user_id: "70f2ef02-9ee7-4062-b801-554ce00bb6a3",
-    user_name: "liuchaofan",
-    display_name: "刘超凡",
-    user_phone: "",
-    user_avator: "",
-    user_pwd: "2019@cestc",
-    user_access: ["examine"],
-    userAccess: "examine",
-    group_id: "4",
-    group_name: "静音检测站",
-    lock_flag: "0"
+    id: "9d974bb4-2386-4bb8-98c5-499b95ad025c",
+    userName: "test",
+    displayName: "检测员",
+    phone: "",
+    roles: [
+      {
+        id: "5"
+      }
+    ],
+    lockFlag: 0
   },
   {
-    id: null,
-    user_id: "57095390-9c42-41d4-ad8b-51f059bc83ac",
-    user_name: "proline_leader",
-    display_name: "产线线长",
-    user_phone: "",
-    user_avator: "",
-    user_pwd: "2019@cestc",
-    user_access: ["proline_leader"],
-    userAccess: "proline_leader",
-    group_id: "6",
-    group_name: "条码站",
-    lock_flag: "0"
+    id: "70f2ef02-9ee7-4062-b801-554ce00bb6a3",
+    userName: "liuchaofan",
+    displayName: "刘超凡",
+    phone: "",
+    roles: [
+      {
+        id: "5"
+      }
+    ],
+    lockFlag: 0
   },
   {
-    id: null,
-    user_id: "61144682-7438-47f1-9570-c7ddf6213832",
-    user_name: "tuwenxuan",
-    display_name: "涂文轩",
-    user_phone: "",
-    user_avator: "",
-    user_pwd: "2019@cestc",
-    user_access: ["examine"],
-    userAccess: "examine",
-    group_id: "3",
-    group_name: "综合检测站",
-    lock_flag: "0"
+    id: "57095390-9c42-41d4-ad8b-51f059bc83ac",
+    userName: "proline_leader",
+    displayName: "产线线长",
+    phone: "",
+    roles: [
+      {
+        id: "4"
+      }
+    ],
+    lockFlag: 0
   },
   {
-    id: null,
-    user_id: "c151b4b4-3bdc-4522-933a-5de6157bf5b1",
-    user_name: "wangmingxue",
-    display_name: "王明雪",
-    user_phone: "",
-    user_avator: "",
-    user_pwd: "2019@cestc",
-    user_access: ["examine"],
-    userAccess: "examine",
-    group_id: "4",
-    group_name: "静音检测站",
-    lock_flag: "0"
+    id: "61144682-7438-47f1-9570-c7ddf6213832",
+    userName: "tuwenxuan",
+    displayName: "涂文轩",
+    phone: "",
+    roles: [
+      {
+        id: "5"
+      }
+    ],
+    lockFlag: 0
   },
   {
-    id: null,
-    user_id: "75d5298a-2ba8-4308-80d7-cbed61ec420d",
-    user_name: "workshop_manager",
-    display_name: "车间主管",
-    user_phone: "",
-    user_avator: "",
-    user_pwd: "2019@cestc",
-    user_access: ["workshop_manager"],
-    userAccess: "workshop_manager",
-    group_id: "5",
-    group_name: "外观检测站",
-    lock_flag: "0"
+    id: "c151b4b4-3bdc-4522-933a-5de6157bf5b1",
+    userName: "wangmingxue",
+    displayName: "王明雪",
+    phone: "",
+    roles: [
+      {
+        id: "5"
+      }
+    ],
+    lockFlag: 0
   },
   {
-    id: null,
-    user_id: "1c66b592-f522-456d-86b3-cdf2191abcb4",
-    user_name: "zhangjiahui",
-    display_name: "张家辉",
-    user_phone: "14566786543",
-    user_avator: "",
-    user_pwd: "2019@cestc",
-    user_access: ["examine"],
-    userAccess: "examine",
-    group_id: "3",
-    group_name: "综合检测站",
-    lock_flag: "0"
+    id: "75d5298a-2ba8-4308-80d7-cbed61ec420d",
+    userName: "workshop_manager",
+    displayName: "车间主管",
+    phone: "",
+    roles: [
+      {
+        id: "3"
+      }
+    ],
+    lockFlag: 0
+  },
+  {
+    id: "1c66b592-f522-456d-86b3-cdf2191abcb4",
+    userName: "zhangjiahui",
+    displayName: "张家辉",
+    phone: "14566786543",
+    roles: [
+      {
+        id: "5"
+      }
+    ],
+    lockFlag: 0
   }
 ];
 
@@ -164,92 +154,92 @@ const roleList = [
     description: "系统管理员，管理管理中心",
     menus: [
       {
-        functionName: "首页概览",
+        title: "首页概览",
         functionId: 1
       },
       {
-        functionName: "车间驾驶舱",
+        title: "车间驾驶舱",
         functionId: 2
       },
       {
-        functionName: "产线驾驶舱",
+        title: "产线驾驶舱",
         functionId: 3
       },
       {
-        functionName: "检测员",
+        title: "检测员",
         functionId: 4
       },
       {
-        functionName: "任务派发",
+        title: "任务派发",
         functionId: 5
       },
       {
-        functionName: "历史任务",
+        title: "历史任务",
         functionId: 6
       },
       {
-        functionName: "查看SOP",
+        title: "查看SOP",
         functionId: 7
       },
       {
-        functionName: "条码打印",
+        title: "条码打印",
         functionId: 8
       },
       {
-        functionName: "检测列表",
+        title: "检测列表",
         functionId: 9
       },
       {
-        functionName: "追溯查询",
+        title: "追溯查询",
         functionId: 10
       },
       {
-        functionName: "异常确认",
+        title: "异常确认",
         functionId: 11
       },
       {
-        functionName: "账号管理",
+        title: "账号管理",
         functionId: 12
       },
       {
-        functionName: "角色管理",
+        title: "角色管理",
         functionId: 13
       },
       {
-        functionName: "组合管理",
+        title: "组合管理",
         functionId: 14
       },
       {
-        functionName: "异常原因",
+        title: "异常原因",
         functionId: 15
       },
       {
-        functionName: "SOP配置",
+        title: "SOP配置",
         functionId: 16
       },
       {
-        functionName: "数据配置",
+        title: "数据配置",
         functionId: 17
       },
       {
-        functionName: "配方配置",
+        title: "配方配置",
         functionId: 18
       },
       {
-        functionName: "设备配置",
+        title: "设备配置",
         functionId: 19
       },
       {
-        functionName: "工位配置",
+        title: "工位配置",
         functionId: 20
       },
       {
-        functionName: "产线配置",
+        title: "产线配置",
         functionId: 21
       }
     ],
     users: ["admin"],
-    id: 1
+    id: "1"
   },
   {
     name: "cestc",
@@ -257,48 +247,48 @@ const roleList = [
     description: "系统工程师，管理配置中心",
     menus: [
       {
-        functionName: "账号管理",
+        title: "账号管理",
         functionId: 12
       },
       {
-        functionName: "角色管理",
+        title: "角色管理",
         functionId: 13
       },
       {
-        functionName: "组合管理",
+        title: "组合管理",
         functionId: 14
       },
       {
-        functionName: "异常原因",
+        title: "异常原因",
         functionId: 15
       },
       {
-        functionName: "SOP配置",
+        title: "SOP配置",
         functionId: 16
       },
       {
-        functionName: "数据配置",
+        title: "数据配置",
         functionId: 17
       },
       {
-        functionName: "配方配置",
+        title: "配方配置",
         functionId: 18
       },
       {
-        functionName: "设备配置",
+        title: "设备配置",
         functionId: 19
       },
       {
-        functionName: "工位配置",
+        title: "工位配置",
         functionId: 20
       },
       {
-        functionName: "产线配置",
+        title: "产线配置",
         functionId: 21
       }
     ],
     users: ["cestc"],
-    id: 2
+    id: "2"
   },
   {
     name: "workshop_manager",
@@ -306,40 +296,40 @@ const roleList = [
     description: "车间主管，管理整个车间",
     menus: [
       {
-        functionName: "车间驾驶舱",
+        title: "车间驾驶舱",
         functionId: 2
       },
       {
-        functionName: "产线驾驶舱",
+        title: "产线驾驶舱",
         functionId: 3
       },
       {
-        functionName: "检测列表",
+        title: "检测列表",
         functionId: 9
       },
       {
-        functionName: "追溯查询",
+        title: "追溯查询",
         functionId: 10
       },
       {
-        functionName: "账号管理",
+        title: "账号管理",
         functionId: 12
       },
       {
-        functionName: "角色管理",
+        title: "角色管理",
         functionId: 13
       },
       {
-        functionName: "组合管理",
+        title: "组合管理",
         functionId: 14
       },
       {
-        functionName: "异常原因",
+        title: "异常原因",
         functionId: 15
       }
     ],
     users: ["workshop_manager"],
-    id: 3
+    id: "3"
   },
   {
     name: "proline_leader",
@@ -347,52 +337,52 @@ const roleList = [
     description: "产线线长，管理单条产线",
     menus: [
       {
-        functionName: "首页概览",
+        title: "首页概览",
         functionId: 1
       },
       {
-        functionName: "产线驾驶舱",
+        title: "产线驾驶舱",
         functionId: 3
       },
       {
-        functionName: "检测员",
+        title: "检测员",
         functionId: 4
       },
       {
-        functionName: "任务派发",
+        title: "任务派发",
         functionId: 5
       },
       {
-        functionName: "历史任务",
+        title: "历史任务",
         functionId: 6
       },
       {
-        functionName: "查看SOP",
+        title: "查看SOP",
         functionId: 7
       },
       {
-        functionName: "条码打印",
+        title: "条码打印",
         functionId: 8
       },
       {
-        functionName: "检测列表",
+        title: "检测列表",
         functionId: 9
       },
       {
-        functionName: "追溯查询",
+        title: "追溯查询",
         functionId: 10
       },
       {
-        functionName: "异常确认",
+        title: "异常确认",
         functionId: 11
       },
       {
-        functionName: "异常原因",
+        title: "异常原因",
         functionId: 15
       }
     ],
     users: ["proline_leader"],
-    id: 4
+    id: "4"
   },
   {
     name: "examine",
@@ -466,7 +456,7 @@ const roleList = [
       }
     ],
     users: ["examine", "liuchaofan", "tuwenxuan", "wangmingxue", "zhangjiahui"],
-    id: 5
+    id: "5"
   }
 ];
 
